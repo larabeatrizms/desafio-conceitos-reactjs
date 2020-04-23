52c77cd25d5b5176485254710da02ffb64a6aa68
Lara <laraamori.machado@gmail.com>
2020-04-23 00:31:51 -0300

    final changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React from "react";
+import api from "./services/api.js";
 
 import "./styles.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Novo repositorio ${Date.now()}`,
+      url:
+        "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/faq-desafios",
+      techs: ["React", "Node.js"],
+    });
+
+    const repositorie = response.data;
+
+    setRepositories([...repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then((response) => {
+      if (response.status === 204) {
+        const repositoryIndex = repositories.findIndex(
+          (repository) => repository.id === id
+        );
+
+        // if (repositoryIndex < 0) {
+        //   return response.status(400).json({ error: "repository Not Found" });
+        // }
+
+        const repositoriesNew = repositories;
+        repositoriesNew.splice(repositoryIndex, 1);
+
+        setRepositories([...repositoriesNew]);
+      }
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repositorie) => (
+          <li key={repositorie.id}>
+            {repositorie.title}
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
